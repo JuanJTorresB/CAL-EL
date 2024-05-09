e627ea81536572aa451b71fdb2e8c37782da5802
JuanJTorresB <juanjtorresbecerra@gmail.com>
2024-05-09 17:32:01 -0500

    Feat Añadi la estructura json de el user

---
+++ Main.py
@@ -0,0 +1,9 @@
+import Modulos.Menus
+
+print("*"*50)
+print("")
+print("BIENVENIDO".center(50))
+print("")
+print("*"*50)
+
+Modulos.Menus
---
+++ Modulos/Menus.py
@@ -0,0 +1,7 @@
+def Menu_Inicial():
+    print("")
+    print("*"*50)
+    print("")
+    print("BIENVENIDO".center(50))
+    print("")
+    print("*"*50)
